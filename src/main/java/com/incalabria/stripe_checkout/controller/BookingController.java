811a361f313a321b97902b4dcb1f890e65b29577
emanuelromeo <emanuel.romeo@hotmail.com>
2025-10-08 11:15:34 +0200

    Updated booking default image

--- src/main/java/com/incalabria/stripe_checkout/controller/BookingController.java
+++ src/main/java/com/incalabria/stripe_checkout/controller/BookingController.java
@@ -43,7 +43,7 @@ public class BookingController {
                         .setProductData(SessionCreateParams.LineItem.PriceData.ProductData.builder()
                                 .setName(booking.getExperience())
                                 .setDescription(booking.getBookingDescription())
-                                .addImage(booking.getImage() != null && !booking.getImage().isEmpty() ? booking.getImage() : "https://ibb.co/mx5t3kD")
+                                .addImage(booking.getImage() != null && !booking.getImage().isEmpty() ? booking.getImage() : "https://i.postimg.cc/nr4ccDXw/sun.png")
                                 .build())
                         .setUnitAmount(amountInCents)
                         .build())
