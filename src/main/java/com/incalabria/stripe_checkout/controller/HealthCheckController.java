25158a987eb5d1125475d731307d0bf8b77faa27
emanuelromeo <emanuel.romeo@hotmail.com>
2025-10-15 00:36:04 +0200

    Add log in HealthCheckController

--- src/main/java/com/incalabria/stripe_checkout/controller/HealthCheckController.java
+++ src/main/java/com/incalabria/stripe_checkout/controller/HealthCheckController.java
@@ -1,5 +1,7 @@
 package com.incalabria.stripe_checkout.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/health")
 public class HealthCheckController {
 
+    private static final Logger log = LoggerFactory.getLogger(HealthCheckController.class);
+
     @GetMapping
     public ResponseEntity<String> healthCheck() {
+        log.info("Server is alive :)");
         return ResponseEntity.ok().build();
     }
 
